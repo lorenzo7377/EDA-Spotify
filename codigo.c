165fe0ac11b77d4435f9e718a0afc08267e68dad
Lorenzo Santos <lorenzodavinci@hotmail.com>
2022-08-09 16:22:42 +0000

    foi escrito o resto do código

--- codigo.c
+++ codigo.c
@@ -2,53 +2,75 @@
 #include <stdlib.h>
 #include <time.h>
 
-void insercao(int n, int * v1, int *v2)
+void insercao(int n, int * VetorNotas, int *VetorMes)
 {
 	for (int j = 1; j < n; ++j) {
-      int x = *(v1 + j);
-      int y = *(v2 + j);
+      int x = *(VetorNotas + j);
+      int y = *(VetorMes + j);
       int i;
-      for (i = j-1; i >= 0 && *(v1 + i) > x; --i){
-        *(v1 + i + 1) = *(v1+i);
-        *(v2 + i + 1) = *(v2+i);
+      for (i = j-1; i >= 0 && *(VetorNotas + i) > x; --i){
+        *(VetorNotas + i + 1) = *(VetorNotas+i);
+        *(VetorMes + i + 1) = *(VetorMes+i);
         }
-      *(v1 + i + 1) = x;
-      *(v2 + i + 1) = y;
+      *(VetorNotas + i + 1) = x;
+      *(VetorMes + i + 1) = y;
    }
    
 }
 
 int main(){
     FILE * arquivo1 = fopen("revisão.txt", "r");
-    FILE * arquivo2 = fopen("ordenação.txt", "w+");
-    int dia, mes, ano, rating;
-    int *v1, *v2;
+    int d, mes, ano, Notas;
+    int *VetorNotas, *VetorMes;
     int i = 0;
     
     while (1)
     {
-        if(fscanf(arquivo1, "%d-%d-%d-%d", &ano, &mes, &dia, &rating) == EOF){break;}
+        if(fscanf(arquivo1, "%d-%d-%d-%d", &ano, &mes, &d, &Notas) == EOF){break;}
         i++;
     }
-    v1 = (int*)calloc(i, sizeof(int));
-    v2 = (int*)calloc(i, sizeof(int));
+    VetorNotas = (int*)calloc(i, sizeof(int));
+    VetorMes = (int*)calloc(i, sizeof(int));
     fseek(arquivo1,0,SEEK_SET);
     i = 0;
     while (1)
     {
-        if(fscanf(arquivo1, "%d-%d-%d-%d", &ano, v2+i, &dia, v1+i) == EOF){break;}
-        //printf("%d-%d-%d\n",i+1,*(v2+i), *(v1+i));
+        if(fscanf(arquivo1, "%d-%d-%d-%d", &ano, VetorMes+i, &d, VetorNotas+i) == EOF){break;}
         i++;
     }
-    insercao(i+1,v2,v1);
-    for (int k = 0; k < i+1; k++)
+    insercao(i+1,VetorMes,VetorNotas);
+    // for (int j = 0; j < i; j++)
+    // {
+    //     printf("%d - %d\n", *(VetorMes+j), *(VetorNotas+j));
+    // }
+    
+    double soma;
+    int posicao = 1;
+    double contador;
+    for (int j = 1; j <= 7; j++)
     {
-        fprintf(arquivo2,"%d-%d\n",*(v1+k),*(v2+k));
+        soma = 0;
+        contador = 0;
+        for (int k = posicao; k < i + 1; k++)
+        {
+            if (*(VetorMes+k) == j){
+                soma += *(VetorNotas+k);
+                contador++;
+                }
+            else
+            {
+                posicao = k;
+                break;
+            }
+        }
+        printf("Média do mês %d: %.2lf\n", j, soma / contador);
+
     }
     
-    free(v1);
-    free(v2);
+    
+    free(VetorNotas);
+    free(VetorMes);
     fclose(arquivo1);
-    fclose(arquivo2);
+
     return 0;
 }
